eb15a07c760e8b49da023ac6aae0fac07d662c29
TKmd23 <kkotolup@gmail.com>
2022-10-03 12:49:25 +0300

    Auto

---
+++ less_2.py
@@ -0,0 +1,31 @@
+class Human:
+    def __init__(self, name="Human"):
+        self.name = name
+
+class Auto:
+    def __init__(self, brand):
+        self.brand = brand
+        self.passengers = []
+
+    # def add_passenger(self, human):
+    def add_passenger(self, *args):
+        # self.passengers.append(human)
+        for passenger in args:
+            self.passengers.append(passenger)
+
+    def print_passengers_names(self):
+        if self.passengers:
+            print(f"Names of {self.brand} passengers:")
+            for passenger in self.passengers:
+                print(passenger.name)
+        else:
+            print(f"There are no passengers in {self.brand}")
+
+nick = Human("Nick")
+kate = Human("Kate")
+car = Auto("Mersedes")
+
+# car.add_passenger(nick)
+# car.add_passenger(kate)
+car.add_passenger(nick, kate)
+car.print_passengers_names()
